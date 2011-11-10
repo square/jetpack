258837acbeef412c86097e91bc7247f680b8ad02
Xavier Shay <xavier@squareup.com>
2011-11-10 08:48:17 -0800

    Provide a script/ci file for Jenkins.

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+gem "rake"
+
 group :test do
   gem "rspec"
   gem "open4"
--- spec/suite.rb
+++ spec/suite.rb
@@ -1,1 +1,1 @@
-Dir["spec/**/*_spec.rb"].each{|f|require f}
+Dir["spec/**/*_spec.rb"].each{|f|require File.expand_path(f) }
