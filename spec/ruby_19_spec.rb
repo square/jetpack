a20ec4845b79ac906c5fb7aa24103b8d61ce4551
Xavier Shay <xavier@squareup.com>
2011-12-23 11:58:12 -0800

    Support 1.9 mode as a ruby_version option in config.

---
+++ spec/ruby_19_spec.rb
@@ -0,0 +1,34 @@
+require "spec_helper"
+
+describe "preflight - bundler and gems in 1.9 mode" do
+  let(:project) { 'has_gems_via_bundler_19' }
+
+  before(:all) do
+    reset
+    rm_rf("spec/sample_projects/#{project}/vendor/bundle")
+    rm_rf("spec/sample_projects/#{project}/vendor/bundler_gem")
+    x!("bin/preflight spec/sample_projects/#{project}")
+  end
+
+  after(:all) do
+    rm_rf("spec/sample_projects/#{project}/vendor/bundle")
+    rm_rf("spec/sample_projects/#{project}/vendor/bundler_gem")
+  end
+
+  describe "gem installation" do
+    it "installed gems are available via Bundler.require" do
+      rake_result = x("cd spec/sample_projects/#{project} && " +
+                      %{bin/ruby -e 'require \\"rubygems\\"; require \\"bundler\\"; Bundler.require; puts Spruz::Bijection.name'})
+      rake_result[:stderr].should     == ""
+      rake_result[:stdout].should     == "Spruz::Bijection\n"
+      rake_result[:exitstatus].should == 0
+    end
+  end
+
+  describe "bin/rake" do
+    it "uses rake version specified in Gemfile" do
+      rake_result = x("spec/sample_projects/#{project}/bin/rake rake_version")
+      rake_result[:stdout].lines.to_a.last.chomp.should == "0.9.2.2"
+    end
+  end
+end
