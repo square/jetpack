625f4793da90b8b80d39448c87f17e9db18c17ff
Steve Conover <steve@squareup.com>
2012-06-12 11:37:59 -0700

    WIP - dest dir - rack_spec green

--- spec/rack_spec.rb
+++ spec/rack_spec.rb
@@ -2,17 +2,18 @@ require "spec_helper"
 require "yaml"
 
 describe "jetpack - web start for rack app" do
+  let(:dest) { "#{TEST_ROOT}/rack_19" }
+
   before(:all) do
     reset
-    @result = x!("bin/jetpack spec/sample_projects/rack_19")
+    @result = x!("bin/jetpack spec/sample_projects/rack_19 #{dest}")
   end
-
   after(:all) do
     reset
   end
-
+  
   it "runs" do
-    pid_to_kill = run_app("spec/sample_projects/rack_19")
+    pid_to_kill = run_app(dest)
     begin
       #HTTP 4443 - intended to be proxied to from something listening on 443
       x!("curl https://localhost:10443/hello --insecure")[:stdout].split("<br/>").first.strip.should == "Hello World"
