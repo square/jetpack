66c71303529659fa7a520e6d2f50374928918895
Steve Conover <steve@squareup.com>
2012-06-12 11:31:18 -0700

    WIP - dest dir - bundler_spec green

--- spec/bundler_spec.rb
+++ spec/bundler_spec.rb
@@ -1,28 +1,25 @@
 require "spec_helper"
 
 describe "jetpack - bundler and gems" do
-
+  let(:dest) { "#{TEST_ROOT}/has_gems_via_bundler" }
+  
   before(:all) do
     reset
-    rm_rf("spec/sample_projects/has_gems_via_bundler/vendor/bundle")
-    rm_rf("spec/sample_projects/has_gems_via_bundler/vendor/bundler_gem")
-    x!("bin/jetpack spec/sample_projects/has_gems_via_bundler")
+    @result = x!("bin/jetpack spec/sample_projects/has_gems_via_bundler #{dest}")
   end
-
   after(:all) do
-    rm_rf("spec/sample_projects/has_gems_via_bundler/vendor/bundle")
-    rm_rf("spec/sample_projects/has_gems_via_bundler/vendor/bundler_gem")
+    reset
   end
 
   describe "presence of the library" do
     it "installed bundler into vendor/bundler_gem." do
-      files = Dir["spec/sample_projects/has_gems_via_bundler/vendor/bundler_gem/**/*.rb"].to_a.map{|f|File.basename(f)}
+      files = Dir["#{dest}/vendor/bundler_gem/**/*.rb"].to_a.map{|f|File.basename(f)}
       files.should include("bundler.rb")
       files.should include("dsl.rb")
     end
 
     it "is not accidentally using bundler from another ruby environment." do
-      rake_result = x("spec/sample_projects/has_gems_via_bundler/bin/rake load_path_with_bundler")
+      rake_result = x("#{dest}/bin/rake load_path_with_bundler")
       load_path_elements = rake_result[:stdout].split("\n").select{|line|line =~ /^--/}
       load_path_elements.length.should >= 3
       invalid_load_path_elements =
@@ -34,7 +31,7 @@ describe "jetpack - bundler and gems" do
     end
 
     it "can be used from a script fed to jruby." do
-      rake_result = x(%{spec/sample_projects/has_gems_via_bundler/bin/ruby -e 'require \\"rubygems\\"; require \\"bundler\\"; puts Bundler::VERSION'})
+      rake_result = x(%{#{dest}/bin/ruby -e 'require \\"rubygems\\"; require \\"bundler\\"; puts Bundler::VERSION'})
       rake_result[:stderr].should     == ""
       rake_result[:stdout].should include("1.1.0")
       rake_result[:exitstatus].should == 0
@@ -43,14 +40,14 @@ describe "jetpack - bundler and gems" do
 
   describe "gem installation" do
     it "installs gems into vendor/bundle" do
-      files = Dir["spec/sample_projects/has_gems_via_bundler/vendor/bundle/**/*.rb"].to_a.map{|f|File.basename(f)}
+      files = Dir["#{dest}/vendor/bundle/**/*.rb"].to_a.map{|f|File.basename(f)}
       files.should include("bijection.rb")
       files.should include("spruz.rb")
       files.length.should > 20
     end
 
     it "installed gems are available via normal require" do
-      rake_result = x("cd spec/sample_projects/has_gems_via_bundler && " +
+      rake_result = x("cd #{dest} && " +
                       %{bin/ruby -e 'require \\"rubygems\\"; require \\"bundler/setup\\"; require \\"spruz/bijection\\"; puts Spruz::Bijection.name'})
       rake_result[:stderr].should     == ""
       rake_result[:stdout].should     == "Spruz::Bijection\n"
@@ -58,7 +55,7 @@ describe "jetpack - bundler and gems" do
     end
 
     it "installed gems are available via Bundler.require" do
-      rake_result = x("cd spec/sample_projects/has_gems_via_bundler && " +
+      rake_result = x("cd #{dest} && " +
                       %{bin/ruby -e 'require \\"rubygems\\"; require \\"bundler\\"; Bundler.require; puts Spruz::Bijection.name'})
       rake_result[:stderr].should     == ""
       rake_result[:stdout].should     == "Spruz::Bijection\n"
@@ -68,7 +65,7 @@ describe "jetpack - bundler and gems" do
 
   describe "bin/rake" do
     it "uses rake version specified in Gemfile" do
-      rake_result = x("spec/sample_projects/has_gems_via_bundler/bin/rake rake_version")
+      rake_result = x("#{dest}/bin/rake rake_version")
       rake_result[:stdout].lines.to_a.last.chomp.should == "0.9.2.2"
     end
   end
@@ -94,11 +91,12 @@ DEPENDENCIES
 
     after do
       FileUtils.rm_f("spec/sample_projects/has_gems_via_bundler_bad_gemfile_lock/Gemfile.lock")
+      reset
     end
 
     it "regenerates the Gemfile.lock and prints out a warning message" do
       File.read("spec/sample_projects/has_gems_via_bundler_bad_gemfile_lock/Gemfile.lock").should_not include("java")
-      jetpack_result = x("bin/jetpack spec/sample_projects/has_gems_via_bundler_bad_gemfile_lock")
+      jetpack_result = x("bin/jetpack spec/sample_projects/has_gems_via_bundler_bad_gemfile_lock #{TEST_ROOT}/has_gems_via_bundler_bad_gemfile_lock")
       jetpack_result[:stderr].gsub("\n", "").squeeze(" ").should include(%{
         WARNING: Your Gemfile.lock does not contain PLATFORM java.
         Automtically regenerating and overwriting Gemfile.lock using jruby
@@ -107,9 +105,9 @@ DEPENDENCIES
       }.gsub("\n", "").squeeze(" "))
       jetpack_result[:exitstatus].should == 0
 
-      File.read("spec/sample_projects/has_gems_via_bundler_bad_gemfile_lock/Gemfile.lock").should include("java")
+      File.read("#{TEST_ROOT}/has_gems_via_bundler_bad_gemfile_lock/Gemfile.lock").should include("java")
 
-      rake_result = x("cd spec/sample_projects/has_gems_via_bundler_bad_gemfile_lock && " +
+      rake_result = x("cd #{TEST_ROOT}/has_gems_via_bundler_bad_gemfile_lock && " +
                       %{bin/ruby -e 'require \\"rubygems\\"; require \\"bundler\\"; Bundler.require; puts Spruz::Bijection.name'})
       rake_result[:stderr].should     == ""
       rake_result[:stdout].should     == "Spruz::Bijection\n"
