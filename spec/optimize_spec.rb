4b6e46aba715c02f347cce3bb739807bca8a2c7f
Steve Conover & Taylor Phillips <github+steve+taylor@squareup.com>
2012-06-14 10:57:14 -0700

    jetpack shouldn't build if it doesn't need to. rsync's project files in the case that it doesn't.
    fix test

--- spec/optimize_spec.rb
+++ spec/optimize_spec.rb
@@ -28,8 +28,41 @@ bin/.rake_runner
 bin/launch
 bin/rake
 bin/ruby
-vendor
 vendor/jruby.jar
 }
+  end
+
+  it "does not build if no files that drive the jetpack run have changed" do
+    x!("bin/jetpack #{src} #{dest}")[:exitstatus].should == 0
+    File.exists?("#{dest}/vendor/jruby.jar").should == true
+    File.exists?("#{dest}/.jetpack-generated").should == true
+
+    #removing a file out of the desintation.
+    #jetpack will not replace it because jetpack noop's
+    rm("#{dest}/vendor/jruby.jar")
+
+    second_run_result = x!("bin/jetpack #{src} #{dest}")
+    second_run_result[:exitstatus].should == 0
+    second_run_result[:stderr].should == "config/jetpack.yml is the same in both directories, skipping build.\n"
+    File.exists?("#{dest}/vendor/jruby.jar").should == false
+  end
+
+  it "syncs project file changes from source to destination even if nothing jetpack-related has changed." +
+     "jetpack build files should remain." do
+    x!("bin/jetpack #{src} #{dest}")[:exitstatus].should == 0
+    File.exists?("#{dest}/vendor/jruby.jar").should == true
+    File.exists?("#{dest}/.jetpack-generated").should == true
+
+    FileUtils.rm("#{src}/Rakefile")
+    File.open("#{src}/newfile", "w"){|f|f<< "new"}
+
+    #removing a file out of the desintation.
+    #jetpack will not replace it because jetpack noop's
+    rm("#{dest}/vendor/jruby.jar")
+
+    x!("bin/jetpack #{src} #{dest}")[:exitstatus].should == 0
+    File.exists?("#{dest}/vendor/jruby.jar").should == false
+    File.exists?("#{dest}/newfile").should == true
+    File.exists?("#{dest}/Rakefile").should == false
   end
 end
